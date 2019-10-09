71abefda2f57f440ad3b3dbe46e56720aab02387
CloudBotScripts <kaden.lothar@yahoo.com>
2019-10-09 12:01:44 -0300

    Add glooth bandit south

---
+++ glooth_bandit_south/actions.py
@@ -0,0 +1,12 @@
+#Load Custom functions
+import sys
+sys.path.append('../scripts/')
+from lib import *
+import global_actions
+
+def waypoint_action(client, action):
+    if action == "check_teleport":
+        check_hunt(client, 'continue', 'skip_teleport', ammo='ammo_name' in client.hunt_config.keys(), time=True)
+
+    else:
+        global_actions.waypoint_action(client, action)
