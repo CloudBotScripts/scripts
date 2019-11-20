fb79bc329a2909c44f57b6d1d34ff97976e5ceba
CloudBotScripts <kaden.lothar@yahoo.com>
2019-11-20 04:27:16 -0300

    Fix sea serpent

--- sea_serpent/actions.py
+++ sea_serpent/actions.py
@@ -1,131 +1,54 @@
-from time import sleep
-from datetime import datetime
-import clipboard
-
-## Options
-hours_leave = [9, 10]
-skill_train = 'club'
-
-## Cap to leave
-cap_leave = 30
-
-## Mana potions to take
-take_mana = 10
-mana_leave = 5
-mana_name = 'mana potion'
-depot_mana_potions = 3
-hotkey_mana_potions = 4
-
-# Load Custom functions
+#Load Custom functions
 import sys
 sys.path.append('../scripts/')
 from lib import *
+import global_actions
 
-persistent_actions = [{'interval':60, 'action':eat_food}]
+## Hotkey levitate
+levitate_down = '-'
+levitate_up = '='
 
 def waypoint_action(client, action):
-    if action == "start":
-      print('Bot started')
+    if action == "check_tasks":
+        if not client.script_options.get('task', False):
+            client.jump_label('skip_tasks')
 
-    elif action == "debug":
-        client.jump_label('debug')
+    elif action == "get_tasks":
+        client.npc_say(['tasks', 'yes', 'promotion', 'tasks', 'sea serpent', 'yes'])
 
-    elif action == "give_fish_captain":
-        client.npc_say(["mission", 'yes', 'bait', 'bait', 'bait', 'bait', 'hunt', 'yes'])
-
-    elif action == "buy_bait":
-        client.buy_items_from_npc(['bait'], [1])
-
-    elif action == "set_bait":
-        container = client.get_container('Backpack')
-        if container is False:
-            print('Backpack not found')
-            client.logout()
-        sleep(0.4)
-        client.use_slot(container, 0)
-        sleep(0.4)
-        client.click_sqm(0, 1)
-        sleep(0.2)
-
-    elif action == "go_up":
-        client.press('d')
-        sleep(0.2)
-
-    elif action == "go_down":
-        client.press('s')
-        sleep(0.2)
-
-    elif action == "telescope":
-        # open server messages
-        client.use_sqm(-1, 0)
-        sleep(0.4)
-        client.hotkey('alt', 'd')
-        sleep(0.2)
-        client.hotkey('tab')
-        sleep(0.2)
-
-        # get last 4 server messages
-        client.hotkey('shift', 'ctrl', 'a')
-        sleep(0.2)
-        client.hotkey('ctrl', 'c')
-        sleep(0.2)
-        s = clipboard.paste().split('\n')
-        last_messages = s[::-1]
-        for message in last_messages:
-            print(message)
-            if 'sea serpent in sight' in message:
-                client.jump_label('telescope')
-                break
-            elif 'you lost it' in message:
-                client.jump_label('telescope')
-                break
-            elif 'straight ahead of you' in message:
-                client.jump_label('straight')
-                break
-            elif 'the starboard side' in message:
-                client.jump_label('starboard')
-                break
-            elif 'the larboard side' in message:
-                client.jump_label('larboard')
-                break
-            elif 'gain speed' in message:
-                client.jump_label('speed')
-                break
-            elif 'right location' in message:
-                client.jump_label('finish')
-                break
-            else:
-                print('Message does not match')
+    elif action == "check_hunt":
+        if client.script_options['respawn'] == 'north':
+            client.jump_label('go_north')
+        elif client.script_options['respawn'] == 'south':
+            client.jump_label('go_south')
         else:
-            print('Failed to get message')
-            exit()
+            client.jump_label('go_south_down')
 
-    elif action == "repeat":
-        client.jump_label('repeat_bait')
+    elif action == "check_north":
+        check_hunt(client, 'hunt_north', 'leave_north', ammo=ammo, time=True)
 
-    elif action == "straight":
-        client.npc_say(["straight"])
+    elif action == "check_south":
+        check_hunt(client, 'hunt_south', 'leave_south', ammo=ammo, time=True)
+            
+    elif action == "check_south_down":
+        check_hunt(client, 'hunt_south_down', 'leave_south_down', ammo=ammo, time=True)
 
-    elif action == "starboard":
-        client.npc_say(["starboard"])
-
-    elif action == "larboard":
-        client.npc_say(["larboard"])
-
-    elif action == "speed":
-        client.npc_say(["speed"])
-
-    elif action == "passage":
-        client.npc_say(["passage", 'yes'])
-
-    elif action == "travel_venore":
-        client.npc_say(["venore", 'yes'])
-
-    elif action == "travel_edron":
-        client.npc_say(["edron", 'yes'])
-
-    elif action == "end":
-        client.logout()
+    elif action == "levitate_south_down":
+        levitate(client, 'south', levitate_down)
+    elif action == "levitate_east_down":
+        levitate(client, 'east', levitate_down)
+    elif action == "levitate_west_down":
+        levitate(client, 'west', levitate_down)
+    elif action == "levitate_north_down":
+        levitate(client, 'north', levitate_down)
+    elif action == "levitate_south_up":
+        levitate(client, 'south', levitate_up)
+    elif action == "levitate_east_up":
+        levitate(client, 'east', levitate_up)
+    elif action == "levitate_west_up":
+        levitate(client, 'west', levitate_up)
+    elif action == "levitate_north_up":
+        levitate(client, 'north', levitate_up)
 
     else:
-        print('Action', action, 'is not defined')
+        global_actions.waypoint_action(client, action)
