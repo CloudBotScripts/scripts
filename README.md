d5f78b9beb51b6e4ed28c5ba5a5a5fc5bc7c6cda
CloudBotScripts <kaden.lothar@yahoo.com>
2019-12-04 23:27:11 -0300

    add meriana script

--- README.md
+++ README.md
@@ -122,7 +122,7 @@ Soon
 | ----- | ------------------------------- | --------------------------------------- | --------------------------------------- |
 | 15+   | [Minotaur Darashia](minotaur_darashia/setup_mage.json)| :star: :star:                           | :star: :star: :star: :star:             |
 | 30+   | Tarantulas Port Hope            | :star:                                  | :star:                                  |
-| 40+   | Meriana Gargoyle Cave           | :star:                                  | :star: :star: :star:                    |
+| 40+   | [Meriana Gargoyle Cave](meriana_gargoyle/setup_mage.json) | :star:                                  | :star: :star: :star: :star:         |
 
 ### Master Sorcerer/Elder Druid (100-200)
 | Level | Name                            | Profit                                  | XP                                      |
--- quests/meriana/actions.py
+++ quests/meriana/actions.py
@@ -1,66 +1,20 @@
-from time import sleep
-from datetime import datetime
-
-## Options
-hours_leave = [9, 10]
-skill_train = 'club'
-
-## Cap to leave
-cap_leave = 30
-
-## Mana potions to take
-take_mana = 10
-mana_leave = 5
-mana_name = 'mana potion'
-depot_mana_potions = 3
-hotkey_mana_potions = 4
-
-# Load Custom functions
 import sys
 sys.path.append('../scripts/')
 from lib import *
-
-persistent_actions = [{'interval':60, 'action':eat_food}]
+import global_actions
 
 def waypoint_action(client, action):
-    if action == "start":
-      print('Bot started')
-
-    elif action == "debug":
-        client.jump_label('debug')
-
-    elif action == "buy_rake":
+    if action == "buy_rake":
         client.buy_items_from_npc(['rake'], [1])
-
-    elif action == "travel_abdendriel":
-        client.npc_say(["ab'dendriel", 'yes'])
 
     elif action == "buy_poem":
         client.npc_say(["love poem", 'yes'])
 
-    elif action == "travel_venore":
-        client.npc_say(["venore", 'yes'])
-
-    elif action == "travel_liberty_bay":
-        client.npc_say(["liberty bay", 'yes'])
-
     elif action == "ring":
         client.npc_say(["ring", 'yes'])
 
-    elif action == "levitate_up_north":
-        levitate(client, 'north', '=')
-
-    elif action == "levitate_down_south":
-        levitate(client, 'south', '-')
-
     elif action == "use_rake_north":
-        container = client.get_container('Backpack')
-        if container is False:
-            client.logout()
-        client.use_slot(container, 1)
-        sleep(0.2)
-        client.click_sqm(0, 1)
-        sleep(0.2)
+        use_item_at_sqm(client, 'rake', (0,1))
 
     elif action == "give_ring":
         client.npc_say(["ring", 'yes', 'yes', 'yes'])
@@ -89,12 +43,8 @@ def waypoint_action(client, action):
     elif action == "djinn2":
         client.npc_say(['mermaid', 'love poem', 'yes'])
 
-
     elif action == "finish":
         client.npc_say(['raymond striker', 'mermaid'])
 
-    elif action == "end":
-        client.logout()
-
     else:
-        print('Action', action, 'is not defined')
+        global_actions.waypoint_action(client, action)
