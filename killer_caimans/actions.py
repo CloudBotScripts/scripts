ebf45a0ff7ecf8a1ab96df81eb9009aaedd5c95d
CloudBotScripts <kaden.lothar@yahoo.com>
2019-12-06 14:39:35 -0300

    Fix killer caimans floors

--- killer_caimans/actions.py
+++ killer_caimans/actions.py
@@ -7,7 +7,18 @@ import global_actions
 def waypoint_action(client, action):
     if action == "use_elevator":
         client.use_lever((1, 0))
-        
+    elif action == "jump_travel_back":
+        client.jump_label('travel_back')
+    elif action == "jump_elevator_gohunt":
+        client.jump_label('elevator_gohunt')
+    elif action == "gohunt_14":
+        conditional_jump_floor(client, 14, 'continue_gohunt_14', 'skip_gohunt_14')
+    elif action == "gohunt_12":
+        conditional_jump_floor(client, 12, 'continue_gohunt_12', 'gohunt_10')
+    elif action == "back_14":
+        conditional_jump_floor(client, 14, 'continue_back_14', 'skip_back_14')
+    elif action == "back_12":
+        conditional_jump_floor(client, 12, 'continue_back_12', 'back_10')
     elif action == "levitate_north_up":
         levitate(client, 'north', client.spells['exani hur up'])
     elif action == "levitate_west_up":
@@ -16,13 +27,6 @@ def waypoint_action(client, action):
         levitate(client, 'south', client.spells['exani hur down'])
     elif action == "levitate_east_down":
         levitate(client, 'east', client.spells['exani hur down'])
-
-    elif action == "refill":
-        if not withdraw_item_from_stash(client, 'brown mushroom', 50, client.items['brown mushroom']): 
-            print('Not enough mushrooms')
-        health_name, take_health = client.hunt_config['health_name'], client.hunt_config['take_health']
-        if not withdraw_item_from_stash(client, health_name, take_health, client.items[health_name]): 
-            print('Not enough health potions')
 
     elif action == "check_down":
         check_hunt(client, 'continue', 'leave', time=True)
